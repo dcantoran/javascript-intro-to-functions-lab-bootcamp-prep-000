6b434c3fe9909d959b33b3bc1ae794081b6741cc
David N/A Cantoran <davidcantoran36@gmail.com>
2019-02-10 19:50:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase()) {
-    return "I can't hear you!";
+  if (string.tUpperCase()) {
+    return "YES INDEED!";
   } 
 }
 
